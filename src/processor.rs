fce5e7bd7cf1eca8d5701c84d78951bf68493e98
Adam Lawrence <adam.ad.lawrence@gmail.com>
2021-05-04 19:50:20 -0400

    Implement cross-program invocation for account ownership transfer

--- src/processor.rs
+++ src/processor.rs
@@ -1,14 +1,15 @@
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    msg,
     program_error::ProgramError,
-    program_pack::{IsInitialized, Pack},
+    msg,
     pubkey::Pubkey,
+    program_pack::{Pack, IsInitialized},
     sysvar::{rent::Rent, Sysvar},
+    program::invoke
 };
 
-use crate::{instruction::EscrowInstruction, error::EscrowError, state::Escrow};
+use crate::{error::EscrowError, instruction::EscrowInstruction, state::Escrow};
 
 pub struct Processor;
 impl Processor {
@@ -73,6 +74,38 @@ impl Processor {
         if escrow_info.is_initialized() {
             return Err(ProgramError::AccountAlreadyInitialized);
         }
+
+        // Now create the Escrow object
+        escrow_info.is_initialized = true;
+        escrow_info.initializer_pubkey = *initializer.key;
+        escrow_info.temp_token_account_pubkey = *temp_token_account.key;
+        escrow_info.initializer_token_to_receive_account_pubkey = *token_to_receive_account.key;
+        escrow_info.expected_amount = amount;
+
+        Escrow::pack(escrow_info, &mut escrow_account.data.borrow_mut())?;
+
+        // Transfer (user space) ownership of the temporary token account to the Program-derived address
+        // Get the token program, then 
+        let (pda, _bump_seed) = Pubkey::find_program_address(&[b"escrow"], program_id);
+        let token_program = next_account_info(account_info_iter)?;
+        let owner_change_instruction = spl_token::instruction::set_authority(
+            token_program.key,
+            temp_token_account.key,
+            Some(&pda),
+            spl_token::instruction::AuthorityType::AccountOwner,
+            initializer.key,
+            &[&initializer.key],
+        )?;
+
+        msg!("Calling the token program to transfer token account ownership...");
+        invoke(
+            &owner_change_instruction,
+            &[
+                temp_token_account.clone(),
+                initializer.clone(),
+                token_program.clone(),
+            ],
+        )?;
 
         Ok(())
     }
